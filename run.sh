a8a781407fcdf80342389bf099af50a43d265046
SoyeonHH <sodus1102@gmail.com>
2022-08-31 01:37:40 +0900

    Set experimental environments

---
+++ run.sh
@@ -0,0 +1,2 @@
+## MOSI setting
+python main.py --dataset mosi --data_path /data1/multimodal/MulT/data --batch_size 128 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100
