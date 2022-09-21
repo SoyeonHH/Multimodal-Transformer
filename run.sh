2fa911c703f9554cd0eda79662313c3f029ff7e1
SoyeonHH <sodus1102@gmail.com>
2022-09-21 17:46:28 +0900

    Add data explore and executable file

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 ## MOSI unaligned
-python main.py --dataset mosi --data_path /home/iknow/workspace/multimodal/data --vonly --aonly --lonly --batch_size 32 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100
+# python main.py --dataset mosi --data_path /data1/multimodal/MulT/data --batch_size 128 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100
 
 ## MOSI aligned
-# python main.py --dataset mosi --data_path /home/iknow/workspace/multimodal/data --vonly --aonly --lonly --batch_size 32 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100 --aligned
+# python main.py --dataset mosi --data_path /data1/multimodal/MulT/data --vonly --aonly --lonly --batch_size 32 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100 --aligned
 
 ## MOSEI setting
-# python main.py --dataset mosei_senti --data_path /home/iknow/workspace/multimodal/data --batch_size 16 --num_heads 10 --embed_dropout 0.3 --attn_dropout 0.1 --out_dropout 0.1 --num_epochs 20
+python main.py --dataset mosei_senti --data_path /data1/multimodal/MulT/data --batch_size 16 --num_heads 10 --embed_dropout 0.3 --attn_dropout 0.1 --out_dropout 0.1 --num_epochs 20
